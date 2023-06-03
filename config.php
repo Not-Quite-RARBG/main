24c9889ea7353142f0a73752b58517e81a4f7f12
SlideMovies <87663026+SlideMovies@users.noreply.github.com>
2023-06-03 22:20:18 +0500

    Add consumet instance url

--- config.php
+++ config.php
@@ -1,5 +1,6 @@
 <?php
   $api_url = /* your API url */;
   $api_key = /* your API key */;
-  $tmdb_api_key = /* your tmdb API key */
+  $tmdb_api_key = /* your tmdb API key */;
+  $consumetInstanceURL = /* Your instance of api.consumet.org */;
 ?>
